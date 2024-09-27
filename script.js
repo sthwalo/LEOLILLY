cbf37d36dd84313ecaf75ba913c24c95c5e28166
Immaculate Nyoni <sthwaloe@gmail.com>
2024-09-27 20:07:09 +0200

    Initial commit with refactored HTML, CSS, and JS for LeoLilly website

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('enrollment-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert("Thank you for enrolling! We will contact you soon.");
+   
+});
