f3c07e825f6828ecbd874dffea30d12d1806fbba
Immaculate Nyoni <sthwaloe@gmail.com>
2024-09-29 08:45:02 +0200

    Added custom Javascript

---
+++ theme/custom.js
@@ -0,0 +1,4 @@
+document.getElementById('enrollment-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert("Thank you for enrolling! We will contact you soon.");
+});
